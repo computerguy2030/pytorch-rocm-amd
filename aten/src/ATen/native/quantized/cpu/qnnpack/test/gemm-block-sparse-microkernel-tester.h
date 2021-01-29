4fcf024bddaecd77265508382f9372002a8c8752
Kimish Patel <kimishpatel@fb.com>
2021-01-29 14:47:39 -0800

    [QNNPACK Sparse] Create fc sparse operator
    Summary:
    Creates sparse operator for fully connected layer.
    
    Test Plan:
    ./build/local/fully-connected-sparse-test
    
    Reviewers:
    
    Subscribers:
    
    Tasks:
    
    Tags:
    
    ghstack-source-id: 8fe35660a97ab0091951ee27c895d4cf2a26c71a
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/50586

--- aten/src/ATen/native/quantized/cpu/qnnpack/test/gemm-block-sparse-microkernel-tester.h
+++ aten/src/ATen/native/quantized/cpu/qnnpack/test/gemm-block-sparse-microkernel-tester.h
@@ -243,7 +243,6 @@ class GemmBlockSparseMicrokernelTester {
 
     for (size_t iteration = 0; iteration < 1; iteration++) {
       std::generate(a.begin(), a.end(), std::ref(u8rng));
-      std::generate(b.begin(), b.end(), std::ref(u8rng));
       std::generate(bias.begin(), bias.end(), std::ref(s32rng));
       std::fill(c.begin(), c.end(), 0.0f);
       size_t num_zero_points_padded = n() + 8;
@@ -261,7 +260,7 @@ class GemmBlockSparseMicrokernelTester {
         min_elem = *std::min_element(b.cbegin(), b.cend());
       } while (max_elem == min_elem);
 
-      BCSRMatrix bcsr_matrix =
+      std::unique_ptr<BCSRMatrix> bcsr_matrix =
         generateBlockCSRMatrix(
             b.data(), n(), k(), blockSize(), kernel_zero_points.data());
 
@@ -309,9 +308,9 @@ class GemmBlockSparseMicrokernelTester {
           n(),
           aPtr,
           aStride() * sizeof(uint8_t),
-          bcsr_matrix.values.data(),
-          bcsr_matrix.row_values.data(),
-          bcsr_matrix.col_indices.data(),
+          bcsr_matrix->values.data(),
+          bcsr_matrix->row_values.data(),
+          bcsr_matrix->col_indices.data(),
           bias.data(),
           c.data(),
           cStride(),
